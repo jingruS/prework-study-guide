e60dca9262b42285a477898f1d9b527e917d6673
JingruS <sunjingru19@gmail.com>
2022-10-24 15:25:12 -0700

    <feature/add-js>

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,26 @@
 var topics = ['HTML', 'CSS','Git','JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var x = 0; x < topics.length; x++){
+function listTopics(){
+ for (var x = 0; x < topics.length; x++){
     console.log(topics[x]);
+  }
 }
-/*var topic = "HTML";
+function selectTopic(){
 
-if (topic === 'HTML') {
+if (randomTopic === 'HTML') {
     console.log("let's study HTML!");
-} else if (topic ==='CSS'){
+} else if (randomTopic ==='CSS'){
     console.log("let's study CSS!");
-} else if (topic === 'Git'){
+} else if (randomTopic === 'Git'){
     console.log("let's study Git!");
-} else if (topic === 'JavaScript'){
+} else if (randomTopic === 'JavaScript'){
     console.log("let's study JavaScript!");
 } else {
     console.log('Please try again!');
+  }
 }
-*/
-
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
